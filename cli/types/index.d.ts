1956f955db757cd4319342f2a98a1df4f0f46c37
Dylan Deruy <dylan@monptivoisinage.com>
2019-06-18 14:39:52 +0200

    revert to a 2.4 typescript definition

--- cli/types/index.d.ts
+++ cli/types/index.d.ts
@@ -4231,9 +4231,8 @@ declare namespace Cypress {
     left: number
   }
 
-  // Diff taken from https://github.com/Microsoft/TypeScript/issues/12215#issuecomment-311923766
-  type Diff<T extends string, U extends string> = ({ [P in T]: P } & { [P in U]: never } & { [x: string]: never })[T]
-  type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
+  type Diff<T extends string, U extends string> = ({ [P in T]: P } & { [P in U]: never } & { [x: string]: never })[T];
+  type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>;
 }
 
 /**
